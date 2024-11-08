2e7fd6781276b12c85458776916215fdabe796b8
andrada radu <irina-andrada.radu@ing.com>
2024-11-08 20:02:44 +0200

    fix: typo + port start and added context path

--- src/main/kotlin/ro/ar/spring/ai/models/OllamaRequest.kt
+++ src/main/kotlin/ro/ar/spring/ai/models/OllamaRequest.kt
@@ -3,6 +3,6 @@ package ro.ar.spring.ai.models
 import com.fasterxml.jackson.annotation.JsonProperty
 
 class OllamaRequest(
-    @JsonProperty("pompt_message") val promptMessage: String,
+    @JsonProperty("prompt_message") val promptMessage: String,
     @JsonProperty("username") val username: String?
 )
